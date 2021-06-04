b7c99874de45c200a94ddeebe9027d5f745fbe76
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 19:01:26 -0600

    Initial Commit

---
+++ TypealiasDemo/TypealiasDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypealiasDemoApp.swift
+//  TypealiasDemo
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TypealiasDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
